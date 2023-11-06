3ef7d89613f2f7f7c17763a4bf494defa790cd06
unknown <saifullahl5210@gmail.com>
2023-11-06 13:51:08 -0500

    nopost

--- app.py
+++ app.py
@@ -13,26 +13,26 @@ app = Flask(__name__)
 def hello():
     return "hey", 200
 
-@app.route('/', methods=['POST'])
-def webhook():
-  data = request.get_json()
-
-  # We don't want to reply to ourselves!
-  if data['name'] != 'The Talker':
-    msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-    send_message(msg)
-
-  return "ok", 200
-
-def send_message(msg):
-  url  = 'https://api.groupme.com/v3/groups/97130171/messages?token=ytitIf5Eb3KKfHcDuT20MGmaTpMEyCBR2CrGmFAd'
-
-  data = {
-          'bot_id' : '7a68edb9635828c387afad07d7',
-          'text'   : msg,
-         }
-  request = requests.post(url, json=data)
-  json = urlopen(request).read().decode()
+# @app.route('/', methods=['POST'])
+# def webhook():
+#   data = request.get_json()
+#
+#   # We don't want to reply to ourselves!
+#   if data['name'] != 'The Talker':
+#     msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+#     send_message(msg)
+#
+#   return "ok", 200
+#
+# def send_message(msg):
+#   url  = 'https://api.groupme.com/v3/groups/97130171/messages?token=ytitIf5Eb3KKfHcDuT20MGmaTpMEyCBR2CrGmFAd'
+#
+#   data = {
+#           'bot_id' : '7a68edb9635828c387afad07d7',
+#           'text'   : msg,
+#          }
+#   request = requests.post(url, json=data)
+#   json = urlopen(request).read().decode()
 
 # import requests
 # from fastapi import FastAPI, Request
