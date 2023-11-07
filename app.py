b45e9ce30f168fa8f76b1fc3348e3b4e2ed0f1b7
unknown <saifullahl5210@gmail.com>
2023-11-07 07:16:07 -0500

    yo

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-
 from urllib.request import urlopen
-
 import requests
 from flask import Flask, request
 
@@ -14,21 +12,16 @@ def hello():
 def webhook():
   data = request.get_json()
 
-  # We don't want to reply to ourselves!
-  if data['name'] != 'The Talker':
-    msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-    send_message(msg)
+  if '.' in data['text'].lower()[0]:
+      response_text = "yo"
+      url = "https://api.groupme.com/v3/bots/post"
+      data = {
+          "bot_id": "cab5b3cf6bcaa4b7db9d482f5b",
+          "text": response_text
+      }
+      response = requests.post(url, json=data)
+      json = urlopen(response).read().decode()
 
-  return "ok", 200
+  return {'status': "OK"}
 
-def send_message(msg):
-  url  = 'https://api.groupme.com/v3/bots/post'
-
-  data = {
-          'bot_id' : '215bfbafb68c975c754166180e',
-          'text'   : msg,
-         }
-  request = requests.post(url, json=data)
-  json = urlopen(request).read().decode()
-  print(json)
 
