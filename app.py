461ca76a3ce9e67406fcdd9f1fb28b1f599932e6
unknown <saifullahl5210@gmail.com>
2023-11-07 22:03:44 -0500

    big change

--- app.py
+++ app.py
@@ -8,43 +8,58 @@ client = OpenAI(api_key=API_KEY)
 
 app = Flask(__name__)
 
-chat_log = [{"role": "system", "content": "Use popular Islamic terms"}]
-
 
 @app.route("/", methods=['GET'])
 def hello():
     return "hey", 200
 
 
+
+chat_log = []
+
 @app.route('/', methods=['POST'])
 def webhook():
     global chat_log
     data = request.get_json()
+    url = "https://api.groupme.com/v3/bots/post"
 
     if '.' in data['text'].lower()[0]:
         text = data['text'].lower()[1:]
-        response_text = chat(text) + " :" + str(len(chat_log))
-        url = "https://api.groupme.com/v3/bots/post"
+
+        chat_log.append({"role": "user", "content": text})
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=chat_log,
+            max_tokens=150
+        )
+        bot_response = response.choices[0].message.content.strip("\n").strip()
+        chat_log.append({"role": "assistant", "content": bot_response})
+
         data = {
             "bot_id": "cab5b3cf6bcaa4b7db9d482f5b",
-            "text": response_text
+            "text": bot_response
         }
+
         response = requests.post(url, json=data)
-        json = urlopen(response).read().decode()
+
     return {'status': "OK"}
+
+
+
+
 
 
 # client.api_key = API_KEY
 
-def chat(text):
-    global chat_log
-    user_message = text
-    chat_log.append({"role": "user", "content": user_message})
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=chat_log,
-        max_tokens=150
-    )
-    bot_response = response.choices[0].message.content.strip("\n").strip()
-    chat_log.append({"role": "assistant", "content": bot_response})
-    return bot_response
+# def chat(text):
+#     global chat_log
+#     user_message = text
+#     chat_log.append({"role": "user", "content": user_message})
+#     response = client.chat.completions.create(
+#         model="gpt-3.5-turbo",
+#         messages=chat_log,
+#         max_tokens=150
+#     )
+#     bot_response = response.choices[0].message.content.strip("\n").strip()
+#     chat_log.append({"role": "assistant", "content": bot_response})
+#     return bot_response
