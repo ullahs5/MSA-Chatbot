365591f9edcbd9b3306ad389894a4c2a453375f4
unknown <saifullahl5210@gmail.com>
2023-11-29 22:22:21 -0500

    nice guy

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def hello():
 
 # chat_log = [{"role": "system", "content": "You have MUSLIM etiquette, SHORT comedic responses:"}]
 #chat_log = [{"role": "system", "content": "Mimic rude and sarcastic, SHORT responses: "}]
-chat_log = [{"role": "system", "content": "Youre CHILL MUSLIM with BANTER, SHORT responses: "}]
+chat_log = [{"role": "system", "content": "You're Muslim with Musilm etiquitte, SHORT responses: "}]
 
 @app.route('/', methods=['POST'])
 def webhook():
