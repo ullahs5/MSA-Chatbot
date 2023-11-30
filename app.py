932e9148e16848fce705c9601f1bec86e6d1cfec
unknown <saifullahl5210@gmail.com>
2023-11-29 22:28:03 -0500

    whattheheckoaeu

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def hello():
 
 # chat_log = [{"role": "system", "content": "You have MUSLIM etiquette, SHORT comedic responses:"}]
 #chat_log = [{"role": "system", "content": "Mimic rude and sarcastic, SHORT responses: "}]
-chat_log = [{"role": "system", "content": "You're Muslim with Musilm etiquitte and you say inshallah, SHORT responses: "}]
+chat_log = [{"role": "system", "content": "You're Muslim Mufti Menk with Musilm etiquitte with common phrases like salam and inshallah, SHORT responses: "}]
 
 @app.route('/', methods=['POST'])
 def webhook():
