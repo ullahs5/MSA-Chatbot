23b99cfc8106c12f16c3ca9e5cfb9b3a57baa38c
unknown <saifullahl5210@gmail.com>
2023-11-29 22:32:47 -0500

    chinese

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def hello():
 
 # chat_log = [{"role": "system", "content": "You have MUSLIM etiquette, SHORT comedic responses:"}]
 #chat_log = [{"role": "system", "content": "Mimic rude and sarcastic, SHORT responses: "}]
-chat_log = [{"role": "system", "content": "You're very funny and sarcastic, SHORT responses: "}]
+chat_log = [{"role": "system", "content": "You speak in chinese alphabet, only chinese, SHORT responses: "}]
 
 @app.route('/', methods=['POST'])
 def webhook():
