d89bad959a749020539263c81e160c19dc168ad8
unknown <saifullahl5210@gmail.com>
2023-12-28 14:45:07 -0500

    noenvid

--- chatbot.py
+++ chatbot.py
@@ -3,27 +3,13 @@ import requests
 from flask import Flask, request
 from openai import OpenAI
 import os
+from messages import local_masjids, jummah_announcment, halal_options
 
 
-API_KEY = os.getenv('api_key')
-client = OpenAI(api_key=API_KEY)
+AI_API_KEY = os.getenv('api_key')
+client = OpenAI(api_key=AI_API_KEY)
 
 app = Flask(__name__)
-
-local_masjids = """Local alternative masjids that will be open for Jummuah are:  
-                - Al-Rahman Mosque - 26 Josie Street, Dayton OH (first one at 1:30 PM, second one at 2:30 PM) 
-                - Al-Huda Mosque - 731 S Alpha Bellbrook Rd, Sugarcreek Township, OH (first one at 1:30 PM) 
-                - Dayton Mercy Center - 2227 Maue Rd, Miamisburg, OH (first one at 1:30 PM, second one at 3:00 PM) 
-                - Islamic Center of Centerville - 10501 Success Ln, Washington Township, OH (first one at 1:30 PM, second one at 2:30)"""
-halal_options = """Halal options on campus (look for halal certificate):  
-                - Kennedy Union - Que chicken tenders and rotisserie chicken
-                - Virginia West Kettering - Passports and Mongolian grill
-                Halal options off campus:
-                -Gyro Palace (select options) - 1124 Brown St, Dayton, OH 45409
-                -Halal Burgers - 767 Lyons Rd, Washington Township, OH 45459
-                -International Cafe & Delicatessen - 263 N Main St, Centerville, OH 45459"""
-
-jummah_announcment = """ Salaam everyone! We have jummah today at 1:30 in the rike center. """
 
 @app.route("/", methods=['GET'])
 def hello():
@@ -57,7 +43,7 @@ def webhook():
         chat_log.append({"role": "assistant", "content": bot_response})
 
         data = {
-            "bot_id": "cab5b3cf6bcaa4b7db9d482f5b",
+            "bot_id": os.getenv('bot_id'),
             "text": bot_response
         }
         response = requests.post(url, json=data)
@@ -70,7 +56,7 @@ def webhook():
             bot_response = halal_options
 
         data = {
-            "bot_id": "cab5b3cf6bcaa4b7db9d482f5b",
+            "bot_id": os.getenv('bot_id'),
             "text": bot_response
         }
         response = requests.post(url, json=data)
@@ -81,7 +67,7 @@ def announcement():
 
     url = "https://api.groupme.com/v3/bots/post"
     data = {
-        "bot_id": "cab5b3cf6bcaa4b7db9d482f5b",
+        "bot_id": os.getenv('bot_id'),
         "text": jummah_announcment
     }
     response = requests.post(url, json=data)
