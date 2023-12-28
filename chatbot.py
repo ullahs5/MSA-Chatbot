0b48d91f7ec4a0643a5ddf65be5e0bacc19da671
unknown <saifullahl5210@gmail.com>
2023-12-28 09:17:26 -0500

    secretfix

--- chatbot.py
+++ chatbot.py
@@ -2,14 +2,27 @@ import datetime
 import requests
 from flask import Flask, request
 from openai import OpenAI
-from secret import api_key, jummah_announcment, halal_options, local_masjids, my_bot_id
 
 
-API_KEY = api_key
+API_KEY = "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"
 client = OpenAI(api_key=API_KEY)
 
 app = Flask(__name__)
 
+local_masjids = """Local alternative masjids that will be open for Jummuah are:  
+                - Al-Rahman Mosque - 26 Josie Street, Dayton OH (first one at 1:30 PM, second one at 2:30 PM) 
+                - Al-Huda Mosque - 731 S Alpha Bellbrook Rd, Sugarcreek Township, OH (first one at 1:30 PM) 
+                - Dayton Mercy Center - 2227 Maue Rd, Miamisburg, OH (first one at 1:30 PM, second one at 3:00 PM) 
+                - Islamic Center of Centerville - 10501 Success Ln, Washington Township, OH (first one at 1:30 PM, second one at 2:30)"""
+halal_options = """Halal options on campus (look for halal certificate):  
+                - Kennedy Union - Que chicken tenders and rotisserie chicken
+                - Virginia West Kettering - Passports and Mongolian grill
+                Halal options off campus:
+                -Gyro Palace (select options) - 1124 Brown St, Dayton, OH 45409
+                -Halal Burgers - 767 Lyons Rd, Washington Township, OH 45459
+                -International Cafe & Delicatessen - 263 N Main St, Centerville, OH 45459"""
+
+jummah_announcment = """ Salaam everyone! We have jummah today at 1:30 in the rike center. """
 
 @app.route("/", methods=['GET'])
 def hello():
@@ -43,7 +56,7 @@ def webhook():
         chat_log.append({"role": "assistant", "content": bot_response})
 
         data = {
-            "bot_id": my_bot_id,
+            "bot_id": "cab5b3cf6bcaa4b7db9d482f5b",
             "text": bot_response
         }
         response = requests.post(url, json=data)
@@ -56,7 +69,7 @@ def webhook():
             bot_response = halal_options
 
         data = {
-            "bot_id": my_bot_id,
+            "bot_id": "cab5b3cf6bcaa4b7db9d482f5b",
             "text": bot_response
         }
         response = requests.post(url, json=data)
