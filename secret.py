57e0c89c7f7b353cd7c6add9f074914b4ee2dc02
unknown <saifullahl5210@gmail.com>
2023-12-28 08:59:36 -0500

    what

---
+++ secret.py
@@ -0,0 +1,16 @@
+api_key = "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"
+my_bot_id = "cab5b3cf6bcaa4b7db9d482f5b"
+local_masjids = """Local alternative masjids that will be open for Jummuah are:  
+                - Al-Rahman Mosque - 26 Josie Street, Dayton OH (first one at 1:30 PM, second one at 2:30 PM) 
+                - Al-Huda Mosque - 731 S Alpha Bellbrook Rd, Sugarcreek Township, OH (first one at 1:30 PM) 
+                - Dayton Mercy Center - 2227 Maue Rd, Miamisburg, OH (first one at 1:30 PM, second one at 3:00 PM) 
+                - Islamic Center of Centerville - 10501 Success Ln, Washington Township, OH (first one at 1:30 PM, second one at 2:30)"""
+halal_options = """Halal options on campus (look for halal certificate):  
+                - Kennedy Union - Que chicken tenders and rotisserie chicken
+                - Virginia West Kettering - Passports and Mongolian grill
+                Halal options off campus:
+                -Gyro Palace (select options) - 1124 Brown St, Dayton, OH 45409
+                -Halal Burgers - 767 Lyons Rd, Washington Township, OH 45459
+                -International Cafe & Delicatessen - 263 N Main St, Centerville, OH 45459"""
+
+jummah_announcment = """ Salaam everyone! We have jummah today at 1:30 in the rike center. """
