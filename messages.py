3c9eacbc1e791eb15353f072e04d97effb89efee
unknown <saifullahl5210@gmail.com>
2023-12-30 19:03:30 -0500

    local env messages

--- messages.py
+++ messages.py
@@ -1,13 +1,6 @@
 import os
 
 local_masjids = os.getenv('local_masjids')
-halal_options = """
-Halal options on campus (look for halal certificate):  
-- Kennedy Union - Que chicken tenders and rotisserie chicken
-- Virginia West Kettering - Passports and Mongolian grill
-Halal options off campus:
--Gyro Palace (select options) - 1124 Brown St, Dayton, OH 45409
--Halal Burgers - 767 Lyons Rd, Washington Township, OH 45459
--International Cafe & Delicatessen - 263 N Main St, Centerville, OH 45459"""
+halal_options = os.getenv('halal_options')
 
-jummah_announcment = """Salaam everyone! We have jummah today at 1:30 in the rike center. """
+jummah_announcement = os.getenv('jummah_announcement')
